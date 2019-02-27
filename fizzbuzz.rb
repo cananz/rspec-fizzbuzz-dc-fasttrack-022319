2c8ffafe7ef56aa41ece6850fcce95dfada55dda
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-02-27 02:22:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,13 +4,8 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
-
   elsif int % 5 == 0
     return "Buzz"
-  
-  elsif (int % 3 == 0) && (int % 5 == 0)
-    return "Fizzbuzz"
-  
   else 
   return nil
 end
